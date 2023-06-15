6239b50f853ef0f88611c486cca1dbd6bbf83422
Jonathan Koh <jonathan.koh75@gmail.com>
2023-06-15 09:39:45 +0800

    Updated planner component
    But far from perfect, will take a step back and think through implementation again

--- client/src/Components/Planner.js
+++ client/src/Components/Planner.js
@@ -6,77 +6,175 @@ import '../sass-files/Planner.scss';
 
 export default function Planner() {
 
+  // const data = [
+  //   {
+  //     type_id: 1,
+  //     type_name: "Income",
+  //     categories: [
+  //       { category_id:1, category_name: "Employment (Jonathan)" },
+  //       { category_id:2, category_name: "Employment (Joy)" },
+  //       { category_id:3, category_name: "Paynow Transfers" }
+  //     ],
+  //     periods: [
+  //       {year: 2023, months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]},
+  //       {year: 2024, months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]}
+  //     ],
+  //     budgets: [
+  //       [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 120, "", 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 120, ""],
+  //       [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 120, "", 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 120, ""],
+  //       [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 120, "", 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 120, ""]
+  //     ],
+  //     total: {
+  //       name: "Total", totals: [30,30,30,30,30,30,30,30,30,30,30,30,360,"", 30,30,30,30,30,30,30,30,30,30,30,30,360,""]
+  //     }
+  //   },
+  //   {
+  //     type_id: 2,
+  //     type_name: "Savings",
+  //     categories: [
+  //       { category_id:1, category_name: "Combined Savings" },
+  //       { category_id:2, category_name: "Gear Savings" }
+  //     ],
+  //     periods: [
+  //       {year: 2023, months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]},
+  //       {year: 2024, months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]}
+  //     ],
+  //     budgets: [
+  //       [20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 240, "", 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 240, ""],
+  //       [20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 240, "", 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 240, ""]
+  //     ],
+  //     total: {
+  //       name: "Total", totals: [40,40,40,40,40,40,40,40,40,40,40,40,480,"", 40,40,40,40,40,40,40,40,40,40,40,40,480,""]
+  //     }
+  //   }
+  // ]
 
-  // const [headerData, setHeaderData] = useState([]);
+
+  // 01. GET TYPES
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchTypes = async () => {
+      try{
+        const res = await axios.get("http://localhost:8800/planner/getTypes");
+        setData(res.data);
+      }catch(err){
+        console.log(err);
+      }
+    }
+    fetchTypes();
+  }, []);
+
+
+  // 02. GET CATEGORIES FOR EACH TYPE
+  data.map(async (type, index) => {
+    try{
+      const res = await axios.get("http://localhost:8800/planner/getCategories/" + type.type_id);
+      data[index] = {...data[index], "categories": res.data}
+    }catch(err){
+      console.log(err);
+    }
+  })
+
+  // 03. GET YEARS
+  const [periods, setPeriods] = useState([]);
+  useEffect(() => {
+    const fetchYears = async () => {
+      try{
+        const res = await axios.get("http://localhost:8800/planner/getYears");
+        setPeriods(res.data)
+      }catch(err){
+        console.log(err);
+      }
+    }
+    fetchYears();
+  }, []);
+
+  // 04. GET MONTH FOR EACH YEAR
+  periods.map(async (period, index) => {
+
+    try{
+
+      const res = await axios.get("http://localhost:8800/planner/getPeriods/" + period.year);
+      periods[index] = {...periods[index], "months":res.data}
+
+    }catch(err){
+      console.log(err);
+    }
+  })
+
+  // 04.5. ADD PERIODS TO DATA
+  data.map((d, index) => {
+    data[index] = {...data[index], "periods": periods}
+  });
+  // console.log("periods: ", data);
+
+
+
+  // YEAR INCREMENT - 'ADD PERIOD'
+  const [maxYear, setMaxYear] = useState();
+  useEffect(() => {
+    const fetchMaxYear = async () => {
+      try{
+        const res = await axios.get("http://localhost:8800/planner/getMaxYear");
+        setMaxYear(res.data[0].max_year)
+      }catch(err){
+        console.log(err);
+      }
+    }
+    fetchMaxYear();
+  }, []);
+
+
+
+
+  function addPeriod(maxYear, month){
+
+    let year = maxYear + 1;
+    month = ("0"+month).slice(-2)
+    let day = "01";
+    let period_id = year + "-" + month + "-" + day
+
+    try{
+
+      console.log(period_id);
+      axios.post("http://localhost:8800/planner/insertPeriod", period_id);
+      // window.location.reload(false);
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+  function handleAddPeriod(){
+
+    console.log("add period clicked", maxYear);
+    for(let i=1; i<13; i++){
+      addPeriod(maxYear, i);
+    }
+
+  }
+
+
+
+
+  // const [rawData, setRawData] = useState([]);
 
   // useEffect(() => {
-  //   const fetchYears = async () => {
+  //   const fetchTableData = async (type_name) => {
   //     try{
-  //       const res = await axios.get("http://localhost:8800/planner/getYears");
-  //       setHeaderData(res.data);
+  //       const res = await axios.get("http://localhost:8800/planner/getTableData/" + type_name);
+  //       setRawData(res.data);
   //     }catch(err){
   //       console.log(err);
   //     }
   //   }
-  //   fetchYears();
+  //   fetchTableData("Income");
   // }, []);
 
+  // console.log(rawData);
 
-  // GET RESPECTIVE MONTHS FOR EACH YEAR
-  // headerData.map(async (item, index) => {
-  //   try{
-  //     const res = await axios.get("http://localhost:8800/planner/getMonths/" + item.year);
-  //     headerData[index] = {...headerData[index], "months":res.data}
-  //   }catch(err){
-  //     console.log(err);
-  //   }
-  // });
+  
 
 
-  //console.log("Header Data: ", headerData);
-
-
-  const data = [
-    {
-      type_id: 1,
-      type_name: "Income",
-      categories: [
-        { category_id:1, category_name: "Employment (Jonathan)" },
-        { category_id:2, category_name: "Employment (Joy)" },
-        { category_id:3, category_name: "Paynow Transfers" }
-      ],
-      periods: [
-        {year: 2023, months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]},
-        {year: 2024, months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]}
-      ],
-      budgets: [
-        [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 120, "", 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 120, ""],
-        [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 120, "", 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 120, ""],
-        [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 120, "", 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 120, ""]
-      ]
-    },
-    {
-      type_id: 2,
-      type_name: "Savings",
-      categories: [
-        { category_id:1, category_name: "Combined Savings" },
-        { category_id:2, category_name: "Gear Savings" }
-      ],
-      periods: [
-        {year: 2023, months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]},
-        {year: 2024, months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]}
-      ],
-      budgets: [
-        [20, 20, 20, 20, 20, 2000.54, 20, 20, 20, 20, 20, 20, 240, "", 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 240, ""],
-        [20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 240, "", 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 240, ""]
-      ]
-    }
-  ]
-
-  // X categories
-  // Y years
-
-  // BUDGETS / X of Sub Arrays / Y*14 VALUES in each Sub Array
 
 
 
@@ -87,7 +185,7 @@ export default function Planner() {
       <p className="headers h4">Planner</p>
 
 
-        {data.map(item => {
+        {/* {data.map(item => {
           console.log(item);
           return(
             <div key={item.type_id} className='type-div'>
@@ -130,13 +228,24 @@ export default function Planner() {
 
               })}
 
-              <div className='table-totals'>
+              <tr className='table-totals'>
+                <td className='row-headers'>{item.total.name}</td>
+                <td className='empty-td'></td>
 
-              </div>
+                {item.total.totals.map(item => {
+                  return(
+                    item === "" ? <td className='empty-td'></td> : <td>{item}</td>
+                  )
+                })}
+
+
+              </tr>
 
             </div>
           )
-        })}
+        })} */}
+
+        <button onClick={handleAddPeriod}>Add Period</button>
 
     </div>
   )
--- server/routes/planner.js
+++ server/routes/planner.js
@@ -146,6 +146,7 @@ router.get("/getMonths/:year", (req,res) => {
 
 
 router.get("/getHeaderData", (req,res) => {
+
     const q = `
         select 
         month(period_id)
@@ -153,6 +154,151 @@ router.get("/getHeaderData", (req,res) => {
         where year(period_id) = ?
     `;
     db.query(q, (err, data) => {
+        if(err){
+            return res.json(err)
+        }else{
+            return res.json(data);
+        }
+    });
+});
+
+
+
+
+
+// 01. GET TYPES
+router.get("/getTypes", (req,res) => {
+
+    const q = `
+        select
+        t.type_id,
+        t.type_name
+        from categories c
+        inner join types t
+        group by t.type_id
+        order by t.type_id
+    `;
+    db.query(q, (err, data) => {
+        if(err){
+            return res.json(err)
+        }else{
+            return res.json(data);
+        }
+    });
+});
+
+// 02. GET CATEGORIES FOR EACH TYPE
+router.get("/getCategories/:type_id", (req,res) => {
+    const type_id = req.params.type_id;
+    const q = `
+        select
+        c.category_id, 
+        c.category_name
+        from categories c
+        where type_id = ?
+    `;
+    db.query(q, [type_id], (err, data) => {
+        if(err){
+            return res.json(err)
+        }else{
+            return res.json(data);
+        }
+    });
+});
+
+// 03. GET YEARS
+router.get("/getYears", (req,res) => {
+
+    const q = `
+        select 
+        year(period_id) as year
+        from budgets
+        group by year;
+    `;
+    db.query(q, (err, data) => {
+        if(err){
+            return res.json(err)
+        }else{
+            return res.json(data);
+        }
+    });
+});
+
+// 04. GET MONTH FOR EACH YEAR
+router.get("/getPeriods/:year", (req,res) => {
+    const year = req.params.year;
+    const q = `
+        select
+        period_id, 
+        left(monthname(period_id), 3) as month_name_short
+        from periods
+        where (year(period_id)) = ?
+    `;
+    db.query(q, [year], (err, data) => {
+        if(err){
+            return res.json(err)
+        }else{
+            return res.json(data);
+        }
+    });
+});
+
+// 04. GET BUDGETS
+router.get("/getPeriods/:year", (req,res) => {
+    const year = req.params.year;
+    const q = `
+        select
+        period_id, 
+        left(monthname(period_id), 3) as month_name_short
+        from periods
+        where (year(period_id)) = ?
+    `;
+    db.query(q, [year], (err, data) => {
+        if(err){
+            return res.json(err)
+        }else{
+            return res.json(data);
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+// YEAR INCREMENT - 'ADD PERIOD'
+// 01. Get highest year
+// 02. Add 1 to the highest year
+// 03. Insert into db for all 12 months
+
+router.get("/getMaxYear", (req,res) => {
+    const q = `
+        select 
+        MAX(year(period_id)) as max_year
+        from periods;
+    `;
+    db.query(q, (err, data) => {
+        if(err){
+            return res.json(err)
+        }else{
+            return res.json(data);
+        }
+    });
+});
+
+router.post("/insertPeriod", (req,res) => {
+    const q = `
+        insert into 
+        periods(period_id)
+        values(?);
+    `
+    const period_id = req.body.period_id
+
+    db.query(q, [period_id], (err, data) => {
         if(err){
             return res.json(err)
         }else{
